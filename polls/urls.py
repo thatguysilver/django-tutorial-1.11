7142c2b7182e410aebacabb277b4f1acbec28ab7
Adam Silver <adampaulsilver@gmail.com>
2017-06-14 02:41:06 -0400

    Writing your first Django app part 4 COMPLETED

--- polls/urls.py
+++ polls/urls.py
@@ -2,7 +2,12 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
+
         url('^$', views.index, name = 'index'),
+        url('^(?P<question_id>[0-9]+)/$', views.detail, name = 'detail'),
+        url('^(?P<question_id>[0-9]+)/$', views.results, name = 'results'),
+        url('^(?P<question_id>[0-9]+)/$', views.results, name = 'vote'),
 
 ]
