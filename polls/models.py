2366e84a52bb2b9db241b1a32b116a497fc31a65
Adam Silver <adampaulsilver@gmail.com>
2017-06-17 22:50:51 -0400

    Writing your first django app, part 7 COMPLETED

--- polls/models.py
+++ polls/models.py
@@ -16,6 +16,11 @@ class Question(models.Model):
     question_text = models.CharField(max_length = 200)
     pub_date = models.DateTimeField('date published')
 
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
+
+
 
 class Choice(models.Model):
     '''ForeignKey tells dj that each choice is related to a single q.
